3d0321849b58657daff4dc493d9e068b1db98857
Boris Poloshkov <bprnd2@yandex.ru>
2023-10-11 19:02:38 +0300

    type hints right before impl

--- src/Euler1.hs
+++ src/Euler1.hs
@@ -8,15 +8,13 @@ module Euler1
     ) where
 
 findSumMultiplies3or5below_rec :: Int -> Int
-findSumMultiplies3or5below_tail :: Int -> Int
-findSumMultiplies3or5below_map :: Int -> Int
-findSumMultiplies3or5below_listgen :: Int -> Int
-findSumMultiplies3or5below_inflist :: Int -> Int
 
 findSumMultiplies3or5below_rec n = findSumMultiplies3or5 (n - 1)
     where 
         findSumMultiplies3or5 0 = 0
         findSumMultiplies3or5 n = findSumMultiplies3or5 (n - 1) + if mod n 3 == 0 || mod n 5 == 0 then n else 0
+
+findSumMultiplies3or5below_tail :: Int -> Int
 
 findSumMultiplies3or5below_tail n 
         | n < 3 = undefined
@@ -26,8 +24,14 @@ findSumMultiplies3or5below_tail n
                 | k == n = s
                 | otherwise = helper n (s + if mod k 3 == 0 || mod k 5 == 0 then k else 0) (k+1)
 
+findSumMultiplies3or5below_map :: Int -> Int
+
 findSumMultiplies3or5below_map n = sum $ map (\x -> if mod x 5 == 0 || mod x 3 == 0 then x else 0) [1..n-1]   
 
+findSumMultiplies3or5below_listgen :: Int -> Int
+
 findSumMultiplies3or5below_listgen n = sum [x | x <- [1..n-1], mod x 3 == 0 || mod x 5 == 0]
+
+findSumMultiplies3or5below_inflist :: Int -> Int
 
 findSumMultiplies3or5below_inflist n = sum $ take (n-1) $ map (\x -> if mod x 5 == 0 || mod x 3 == 0 then x else 0) [1..]
--- src/Euler30.hs
+++ src/Euler30.hs
@@ -9,39 +9,45 @@ module Euler30
      solveListGen) where
 
 powInt :: Int -> Int -> Int
-toDigits :: Int -> [Int]
-sumPower :: Int -> [Int] -> Int
-
-solveRec :: Int -> Int -> Int
-solveRecTail :: Int -> Int -> Int
-solveMap :: Int -> Int -> Int
-solveInfList :: Int -> Int -> Int
-solveListGen :: Int -> Int -> Int
 
 powInt a n
     | n == 0 = 1
     | otherwise = a * powInt a (n - 1)
 
+toDigits :: Int -> [Int]
+
 toDigits x
     | x == 0 = []
     | otherwise = toDigits (div x 10) ++ [mod x 10]
+
+sumPower :: Int -> [Int] -> Int
 
 sumPower p arr
     | null arr = 0
     | otherwise = powInt (last arr) p + sumPower p (init arr)
 
+solveRec :: Int -> Int -> Int
+
 solveRec p n
         | n == 10 = 0
         | otherwise = solveRec p (n - 1) + if sumPower p (toDigits n) == n then n else 0
+
+solveRecTail :: Int -> Int -> Int
 
 solveRecTail p n = helper p n 0
     where
         helper _ 10 s = s
         helper p n s = helper p (n - 1) (s + if n == sumPower p (toDigits n) then n else 0)
 
+solveMap :: Int -> Int -> Int
+
 solveMap p n = sum $ map snd (filter (uncurry (==)) (map (\x -> (x, sumPower p (toDigits x))) [10..n]))
 
+solveListGen :: Int -> Int -> Int
+
 solveListGen p n = sum [x | x <- [10..n-1], sumPower p (toDigits x) == x]
+
+solveInfList :: Int -> Int -> Int
 
 solveInfList p n = sum $ map snd $ filter (uncurry (==)) $ take n $ map (\x -> (x, sumPower p (toDigits x))) [10..]
 
