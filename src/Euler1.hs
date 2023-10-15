28c5b59c960c6a5b1bd6611f62d4320941571b7c
Boris Poloshkov <bprnd2@yandex.ru>
2023-10-15 21:51:03 +0300

    replace guards w/ pattern matching, rm shadowing

--- src/Euler1.hs
+++ src/Euler1.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
-
 module Euler1
   ( findSumMultiplies3or5below_rec,
     findSumMultiplies3or5below_tail,
@@ -13,16 +11,12 @@ findSumMultiplies3or5below_rec :: Int -> Int
 findSumMultiplies3or5below_rec n = findSumMultiplies3or5 (n - 1)
   where
     findSumMultiplies3or5 0 = 0
-    findSumMultiplies3or5 n = findSumMultiplies3or5 (n - 1) + if mod n 3 == 0 || mod n 5 == 0 then n else 0
+    findSumMultiplies3or5 n0 = findSumMultiplies3or5 (n0 - 1) + if mod n0 3 == 0 || mod n0 5 == 0 then n0 else 0
 
 findSumMultiplies3or5below_tail :: Int -> Int
-findSumMultiplies3or5below_tail n
-  | n < 3 = undefined
-  | otherwise = helper n 0 3
+findSumMultiplies3or5below_tail n = if n >= 3 then helper n 0 3 else undefined
   where
-    helper n s k
-      | k == n = s
-      | otherwise = helper n (s + if mod k 3 == 0 || mod k 5 == 0 then k else 0) (k + 1)
+    helper n0 s k = if k == n0 then s else helper n0 (s + if mod k 3 == 0 || mod k 5 == 0 then k else 0) (k + 1)
 
 findSumMultiplies3or5below_map :: Int -> Int
 findSumMultiplies3or5below_map n = sum $ map (\x -> if mod x 5 == 0 || mod x 3 == 0 then x else 0) [1 .. n - 1]
--- src/Euler30.hs
+++ src/Euler30.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
-
 -- Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 -- Since 6*9^5 = 354929 < 999999 - maximum 6-digit number, there is no point in searching above this limit
 module Euler30
@@ -12,30 +10,26 @@ module Euler30
 where
 
 powInt :: Int -> Int -> Int
-powInt a n
-  | n == 0 = 1
-  | otherwise = a * powInt a (n - 1)
+powInt _ 0 = 1
+powInt a n = a * powInt a (n - 1)
 
 toDigits :: Int -> [Int]
-toDigits x
-  | x == 0 = []
-  | otherwise = toDigits (div x 10) ++ [mod x 10]
+toDigits 0 = []
+toDigits x = toDigits (div x 10) ++ [mod x 10]
 
 sumPower :: Int -> [Int] -> Int
-sumPower p arr
-  | null arr = 0
-  | otherwise = powInt (last arr) p + sumPower p (init arr)
+sumPower _ [] = 0
+sumPower p arr = powInt (last arr) p + sumPower p (init arr)
 
 solveRec :: Int -> Int -> Int
-solveRec p n
-  | n == 10 = 0
-  | otherwise = solveRec p (n - 1) + if sumPower p (toDigits n) == n then n else 0
+solveRec _ 10 = 0
+solveRec p n = solveRec p (n - 1) + if sumPower p (toDigits n) == n then n else 0
 
 solveRecTail :: Int -> Int -> Int
 solveRecTail p n = helper p n 0
   where
     helper _ 10 s = s
-    helper p n s = helper p (n - 1) (s + if n == sumPower p (toDigits n) then n else 0)
+    helper p0 n0 s = helper p0 (n0 - 1) (s + if n0 == sumPower p0 (toDigits n0) then n0 else 0)
 
 solveMap :: Int -> Int -> Int
 solveMap p n = sum $ map snd (filter (uncurry (==)) (map (\x -> (x, sumPower p (toDigits x))) [10 .. n]))
